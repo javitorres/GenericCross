026c5bdb5ab75a4c75ed9161fbfa91bde1f21f37
jtorres <jtorres@madiva.com>
2023-11-10 20:01:47 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 3000; // Puedes usar cualquier puerto que esté disponible
+
+// Middleware para servir archivos estáticos
+app.use(express.static('public')); // 'public' es el nombre de la carpeta donde tienes tu HTML
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html')); // Asegúrate de que el path coincida con la ubicación de tu archivo HTML
+});
+
+app.get('/coches', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/coches.html')); // Asegúrate de que el path coincida con la ubicación de tu archivo HTML
+  });
+
+app.get('/cpuExample', (req, res) => {
+res.sendFile(path.join(__dirname, '/public/cpuExample.html')); // Asegúrate de que el path coincida con la ubicación de tu archivo HTML
+});
+
+app.get('/generic', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/generic.html')); // Asegúrate de que el path coincida con la ubicación de tu archivo HTML
+    });
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
