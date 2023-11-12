5855766733b3c23bd3ec66939793eaf7ee852bc7
jtorres <jtorres@madiva.com>
2023-11-12 14:28:06 +0100

    Added scatter plot for two vars analysis

--- public/js/genericCross.js
+++ public/js/genericCross.js
@@ -1,27 +1,15 @@
-
-
-function createChartContainer(chartConfig, index) {
-    var elementId = 'chart-' + chartConfig.columnName;
+function createChartContainer(title, identifier, index) {
+    var elementId = 'chart-' + (Array.isArray(identifier) ? identifier.join("-") : identifier);
     var container = document.createElement('div');
     container.className = 'chart-container column is-half';
     container.id = elementId;
 
-    // Crear el contenedor principal del gráfico
-    var container = document.createElement('div');
-    container.className = 'chart-container column is-half';
-    container.id = elementId;
-
-    // Crear el encabezado del gráfico
     var header = document.createElement('header');
     header.className = 'card-header';
-
-    // Crear el título del gráfico
-    var title = document.createElement('p');
-    title.className = 'card-header-title';
-    title.innerText = chartConfig.title;
-
-    // Añadir el título al encabezado y el encabezado al contenedor
-    header.appendChild(title);
+    var titleElement = document.createElement('p');
+    titleElement.className = 'card-header-title';
+    titleElement.innerText = title;
+    header.appendChild(titleElement);
     container.appendChild(header);
 
     if (index % 2 === 0) {
@@ -33,17 +21,22 @@ function createChartContainer(chartConfig, index) {
 
     var rowContainer = document.getElementById('row-' + Math.floor(index / 2));
     rowContainer.appendChild(container);
+
     return elementId;
 }
 
-function generateChart(chartConfig, dimension, group, elementId) {
+function generateChart(chartConfig, dimension, group, index) {
+    var elementId = createChartContainer(chartConfig.title, chartConfig.columnName, index);
     var chart;
+
     switch (chartConfig.type) {
         case 'numerical':
             chart = dc.barChart('#' + elementId)
                 .dimension(dimension)
                 .group(group)
-                .x(d3.scaleLinear().domain([0, d3.max(group.all(), d => d.key)]));
+                .x(d3.scaleLinear().domain([0, d3.max(group.all(), d => d.key)]))
+                .elasticY(true)
+                .elasticX(true);
             break;
         case 'categorical':
             chart = dc.pieChart('#' + elementId)
@@ -54,7 +47,9 @@ function generateChart(chartConfig, dimension, group, elementId) {
             chart = dc.lineChart('#' + elementId)
                 .dimension(dimension)
                 .group(group)
-                .x(d3.scaleTime().domain(d3.extent(group.all(), d => d.key)));
+                .x(d3.scaleTime().domain(d3.extent(group.all(), d => d.key)))
+                .elasticY(true)
+                .elasticX(true);
             break;
         default:
             throw new Error('Tipo no soportado');
@@ -70,8 +65,32 @@ function generateChart(chartConfig, dimension, group, elementId) {
         resetLink.style.display = chart.filters().length > 0 ? 'block' : 'none';
     });
 
+    chart.on('filtered', function(chart) {
+        updateFiltersSummary();
+    });
+
     return chart;
 }
+
+function updateFiltersSummary() {
+    var filtersList = dc.chartRegistry.list().reduce(function (acc, chart) {
+        var filters = chart.filters();
+        if (filters.length > 0) {
+            acc.push({chart: chart.anchor(), filters: filters});
+        }
+        return acc;
+    }, []);
+
+    var filtersListElement = document.getElementById('filters-list');
+    filtersListElement.innerHTML = ''; // Limpiar el listado actual
+
+    filtersList.forEach(function(item) {
+        var li = document.createElement('li');
+        li.textContent = item.chart + ': ' + item.filters.join(', ');
+        filtersListElement.appendChild(li);
+    });
+}
+
 
 function resetChart(elementId) {
     var chart = dc.chartRegistry.list().find(c => c.anchor() === '#' + elementId);
@@ -81,9 +100,9 @@ function resetChart(elementId) {
     }
 }
 
-function createDataTable(ndx, config) {
+function createDataTable(crossFilterData, config) {
     var dataTable = dc.dataTable("#data-table");
-    var allDim = ndx.dimension(d => d);
+    var allDim = crossFilterData.dimension(d => d);
 
     var columns = config.charts.map(chartConfig => {
         return {
@@ -98,7 +117,6 @@ function createDataTable(ndx, config) {
         .sortBy(d => d[columns[0].label]) // Ordenar por la primera columna como predeterminado
         .order(d3.ascending)
         .on('renderlet', function(table) {
-            // Añadir evento click a cada cabecera para controlar la ordenación
             table.selectAll('.dc-table th')
                 .on('click', function(d) {
                     var column = d.label;
@@ -110,17 +128,51 @@ function createDataTable(ndx, config) {
                     dataTable.sortBy(d => d[column]);
                     dataTable.redraw();
                 });
-                table.selectAll('.dc-table').classed('dc-table', true);
+            table.selectAll('.dc-table').classed('dc-table', true);
         });
 
-        // Intenta aplicar la clase directamente después de que dc.js renderiza la tabla
-        setTimeout(function() {
-            d3.selectAll('#data-table .dc-data-table').classed('dc-table', true);
-        }, 0);
+    setTimeout(function() {
+        d3.selectAll('#data-table .dc-data-table').classed('dc-table', true);
+    }, 0);
 
     return dataTable;
 }
 
+function createRelationChart(relationConfig, crossFilterData, index, data) {
+    if (!Array.isArray(relationConfig.fields)) {
+        console.error("Error: 'fields' should be an array", relationConfig.fields);
+        return;
+    }
+
+    var elementId = createChartContainer(relationConfig.title, relationConfig.fields, index);
+    var dimension = crossFilterData.dimension(d => [d[relationConfig.fields[0]], d[relationConfig.fields[1]]]);
+    var group = dimension.group();
+
+    var chart = dc.scatterPlot('#' + elementId)
+        .dimension(dimension)
+        .group(group)
+        .x(d3.scaleLinear().domain([0, d3.max(data, d => d[relationConfig.fields[0]])]))
+        .y(d3.scaleLinear().domain([0, d3.max(data, d => d[relationConfig.fields[1]])]))
+        .elasticX(true)
+        .elasticY(true);
+    
+    var resetLink = document.createElement('a');
+    resetLink.className = 'reset-link button is-small';
+    resetLink.innerText = 'Reset';
+    resetLink.href = 'javascript:resetChart("' + elementId + '")';
+    document.getElementById(elementId).appendChild(resetLink);
+
+    chart.on('renderlet', function(chart) {
+        resetLink.style.display = chart.filters().length > 0 ? 'block' : 'none';
+    });
+
+    chart.on('filtered', function(chart) {
+        updateFiltersSummary();
+    });
+
+
+    return chart;
+}
 
 function initializeDashboard(config) {
     d3.csv(config.data).then((data) => {
@@ -134,16 +186,23 @@ function initializeDashboard(config) {
             });
         });
 
-        var ndx = crossfilter(data);
+        var crossFilterData = crossfilter(data);
         var charts = config.charts.map((chartConfig, index) => {
-            var elementId = createChartContainer(chartConfig, index);
-            var dimension = ndx.dimension(dc.pluck(chartConfig.columnName));
-            var group = dimension.group().reduceCount();
-            return generateChart(chartConfig, dimension, group, elementId);
+            return generateChart(chartConfig, crossFilterData.dimension(dc.pluck(chartConfig.columnName)), crossFilterData.dimension(dc.pluck(chartConfig.columnName)).group().reduceCount(), index);
         });
 
-        var dataTable = createDataTable(ndx, config);
+        if (config.relations) {
+            config.relations.forEach((relationConfig, index) => {
+                createRelationChart(relationConfig, crossFilterData, charts.length + index, data);
+            });
+        }
 
+        var dataTable = createDataTable(crossFilterData, config);
         dc.renderAll();
+
+        document.getElementById('reset-all').addEventListener('click', function() {
+            dc.filterAll();
+            dc.redrawAll();
+        });
     });
 }
